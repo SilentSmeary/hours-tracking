ad65f78c90fd6a301f532ef4d7dcd49b5961ceff
James <barowikajames@gmail.com>
2024-12-05 01:15:17 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
 <body>
 <ul class="navbar_main">
     <li class="navbar_li"><a class="active" href="index.html">Home</a></li>
-    <li class="navbar_li"><a href="index.html">Contact</a></li>
-    <li class="navbar_li" style="float:right"><a class="active" href="sign_in.html">Login In</a></li>
+    <li class="navbar_li"><a href="contact.html">Contact</a></li> <li class="navbar_li" style="float:right"><a class="active" href="sign_in.html">Login In</a></li>
 </ul>
 
 <div id="container">
@@ -21,23 +20,19 @@
         <h1>Hours Tracking System</h1>
     </div>
     <div id="content">
-        <p>At GibJohn Tutoring, we believe that every student has the potential to excel. Our mission is to provide high-quality, personalized tutoring services that empower students to achieve their academic and personal goals. With a team of experienced and passionate educators, we offer a wide range of tutoring services tailored to meet the individual needs of each learner.
-            <br>
-            <br>
             <u><b>What features does the website have:</b></u>
         <ul>
             <li class="homepage_list">Students are able to log hours with an activity description to ensure that the placements are tracked correctly and that all the work is completed.</li>
-            <li class="homepage_list">As a teacher you are able to view and add students to each class via a CSV. You are able to view each student in the group and see a detailed view of each student. View approved hours and any hours that have been left unapproved, see any hours that have been rejected and the reason that they were rejected. You are also able to reject the hours and leave a feedback on why they were rejected. View overall class projects and see and notes that were left by the Bussiness Mentors. Generate a summary for the students to see how many hours are left on the placements.</li>
+            <li class="homepage_list">As a teacher you are able to view and add students to each class via a CSV. You are able to view each student in the group and see a detailed view of each student. View approved hours and any hours that have been left unapproved, see any hours that have been rejected and the reason why they were rejected. You are also able to reject the hours and leave feedback on why they were rejected. View overall class projects and see and notes that were left by the Business Mentors. Generate a summary for the students to see how many hours are left on the placements.</li>
             <li class="homepage_list">As a School Administrator you are able to manage staff and business data to keep the systems updated.</li>
-            <li class="homepage_list">As a Trust Administrator you are able to manage all of the schools in your trust, do things such as adding or removing schools in the trust, View Placement hours across the all the schools, Generate Trust-Wide Reports, Generate a single report per school, Set the School Administrator accounts for each school in the trust</li>
-            <li class="homepage_list">Bussiness Mentor, You are able to view and manage the students that you are listed as managing. This means that your able to view a list of the students that you have on placement. Approve, reject and adjust the logged hours to ensure accuracy as well as view there work log book for the logged hours to see and track what they are doing on placement.</li>
-            <li class="homepage_list">As a Bussiness Manager/Representative, you are able to see and overview of all working within my organisation ensuring that compliance across the bussiness. You are able to view a list of all the students that are on placement, and then see a detailed view of independant students that looking at there work book etc, You are also able to review the hours that any student has submitted. Aswell as manage the mentor permissions for each student.</li>
+            <li class="homepage_list">As a Trust Administrator you are able to manage all of the schools in your trust, do things such as adding or removing schools in the trust, View Placement hours across all the schools, Generate Trust-Wide Reports, Generate a single report per school, Set the School Administrator accounts for each school in the trust</li>
+            <li class="homepage_list">Business Mentor, You are able to view and manage the students that you are listed as managing. This means you're able to view a list of the students that you have on placement. Approve, reject and adjust the logged hours to ensure accuracy as well as view there work logbook for the logged hours to see and track what they are doing on placement.</li>
+            <li class="homepage_list">As a Business Manager/Representative, you are able to see an overview of all working within your organisation ensuring that compliance across the business. You are able to view a list of all the students that are on placement, and then see a detailed view of independent students that looking at their workbooks etc, You are also able to review the hours that any student has submitted. As well as manage the mentor permissions for each student.</li>
         </ul>
         Sign Up today trusts and Schools are welcome!</p>
         <br>
-        <a id="join_now" href="student_sign_up.html">Trust Sign Up</a>
-        <a id="join_now2" href="tutor_sign_up.html">School Sign Up</a>
-
+        <a id="join_now" href="trust_sign_up.html">Trust Sign Up</a>
+        <a id="join_now2" href="school_sign_up.html">School Sign Up</a>
     </div>
     <img id="content_image" src="assets/home_page_image.png" alt="">
 
