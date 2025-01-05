29e311914c77f771795c9095b49d7be797a27f24
James <barowikajames@gmail.com>
2025-01-05 02:35:20 +0000

    added: basic index

--- index.php
+++ index.php
@@ -1,41 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/style/style.css">
-    <title>Hours Tracking</title>
-</head>
-<body>
-<ul class="navbar_main">
-    <li class="navbar_li"><a class="active" href="index.html">Home</a></li>
-    <li class="navbar_li"><a href="contact.html">Contact</a></li> 
-    <li class="navbar_li" style="float:right"><a class="active" href="sign_in.html">Login</a></li>
-</ul>
+<?php
+session_start();
 
-<div id="container">
-    <div id="heading">
-        <h1>Hours Tracking System</h1>
-    </div>
-    <div id="content">
-            <u><b>What features does the website have:</b></u>
-        <ul>
-            <li class="homepage_list">Students are able to log hours with an activity description to ensure that the placements are tracked correctly and that all the work is completed.</li>
-            <li class="homepage_list">As a teacher you are able to view and add students to each class via a CSV. You are able to view each student in the group and see a detailed view of each student. View approved hours and any hours that have been left unapproved, see any hours that have been rejected and the reason why they were rejected. You are also able to reject the hours and leave feedback on why they were rejected. View overall class projects and see and notes that were left by the Business Mentors. Generate a summary for the students to see how many hours are left on the placements.</li>
-            <li class="homepage_list">As a School Administrator you are able to manage staff and business data to keep the systems updated.</li>
-            <li class="homepage_list">As a Trust Administrator you are able to manage all of the schools in your trust, do things such as adding or removing schools in the trust, View Placement hours across all the schools, Generate Trust-Wide Reports, Generate a single report per school, Set the School Administrator accounts for each school in the trust</li>
-            <li class="homepage_list">Business Mentor, You are able to view and manage the students that you are listed as managing. This means you're able to view a list of the students that you have on placement. Approve, reject and adjust the logged hours to ensure accuracy as well as view there work logbook for the logged hours to see and track what they are doing on placement.</li>
-            <li class="homepage_list">As a Business Manager/Representative, you are able to see an overview of all working within your organisation ensuring that compliance across the business. You are able to view a list of all the students that are on placement, and then see a detailed view of independent students that looking at their workbooks etc, You are also able to review the hours that any student has submitted. As well as manage the mentor permissions for each student.</li>
-        </ul>
-        Sign Up today trusts and Schools are welcome!</p>
-        <br>
-        <a id="join_now" href="trust_sign_up.html">Trust Sign Up</a>
-        <a id="join_now2" href="school_sign_up.html">School Sign Up</a>
-    </div>
-    <img id="content_image" src="assets/home_page_image.png" alt="">
-</div>
-</body>
-</html>
+include "server/db_connect.php";
+include "server/audit.php";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$role = $_POST['role'];
+
+## REFACTOR: Make function that is reuseable with only needing to change the $role
+
+if ($role == "student") {
+    $sql = "SELECT students.student_id, students.first_name, students.last_name, students.email, students.password, students.course_id, courses.course_name, courses.hours
+            FROM students
+            INNER JOIN courses ON students.course_id = courses.course_id
+            WHERE students.email = ?";
+    $stmt = $conn->prepare($sql); //prepares
+    $stmt->bindParam(1,$email);  //binds the parameters to execute
+    $stmt->execute(); //run the sql code
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);  //brings back results
+
+    if($result) {  // if there is a result returned
+        $_SESSION["s_email"] = $_POST['email'];
+        $_SESSION["s_first_name"] = $result["first_name"];
+        $_SESSION["s_student_id"] = $result["student_id"];
+        $_SESSION["course_id"] = $result["course_id"];
+        $_SESSION["course_name"] = $result["course_name"];
+        $_SESSION["course_hours"] = $result["hours"];
+        if (password_verify($password, $result["password"])) { // verifies the password is matched
+            $_SESSION["ssnlogin"] = true;  // sets up the session variables
+            auditor($role, $result["student_id"],"Logged in successfully");
+            header("location:student/student_home.php");  //redirect on success
+            exit();
+        }
+    }
+} else($role == "teacher"); {
+    $sql = "SELECT * FROM students WHERE email = ?"; //set up the sql statement
+    $stmt = $conn->prepare($sql); //prepares
+    $stmt->bindParam(1,$email);  //binds the parameters to execute
+    $stmt->execute(); //run the sql code
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);  //brings back results
+
+    if($result) {  // if there is a result returned
+        $_SESSION["s_email"] = $_POST['email'];
+        $_SESSION["s_fname"] = $result["first_name"];
+        $_SESSION["s_student_id"] = $result["student_id"];
+        if (password_verify($password, $result["password"])) { // verifies the password is matched
+            $_SESSION["ssnlogin"] = true;  // sets up the session variables
+            auditor($role, $result["student_id"],"Logged in successfully");
+            header("location:list.php");  //redirect on success
+            exit();
+        }
+    }
+}
